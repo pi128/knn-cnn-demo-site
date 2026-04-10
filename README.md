09d0741a38aaf77be6aad32699c1549df0b9168e
James Widner <77074839+pi128@users.noreply.github.com>
2026-04-10 16:25:33 -0400

    Add GitHub Pages demo site for KNN CNN project

---
+++ README.md
@@ -0,0 +1,11 @@
+# KNN CNN Demo Site
+
+Static GitHub Pages version of the project demo.
+
+Files:
+
+- `index.html`
+- `knn_cnn_demo_pool.js`
+- `knn_cnn_outputs/demo_pool/...`
+
+This site shows real validation images and the real cached predictions from the KNN on pretrained ResNet18 embeddings run.
---
+++ knn_cnn_demo_pool.js
@@ -0,0 +1,578 @@
+window.KNN_CNN_DEMO_POOL = [
+  {
+    "image": "knn_cnn_outputs/demo_pool/001_Flag_Smut.jpg",
+    "actual": "Flag Smut",
+    "predicted": "Flag Smut",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/002_Army_worm.jpg",
+    "actual": "Army worm",
+    "predicted": "Army worm",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/003_Wheat_black_rust.jpg",
+    "actual": "Wheat black rust",
+    "predicted": "Wheat black rust",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/004_Leaf_Curl.jpg",
+    "actual": "Leaf Curl",
+    "predicted": "Leaf Curl",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/005_Cotton_Aphid.jpg",
+    "actual": "Cotton Aphid",
+    "predicted": "Cotton Aphid",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/006_red_cotton_bug.jpg",
+    "actual": "red cotton bug",
+    "predicted": "red cotton bug",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/007_Wilt.jpg",
+    "actual": "Wilt",
+    "predicted": "Wilt",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/008_thirps_on__cotton.jpg",
+    "actual": "thirps on  cotton",
+    "predicted": "thirps on  cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/009_RedRust_sugarcane.jpg",
+    "actual": "RedRust sugarcane",
+    "predicted": "RedRust sugarcane",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/010_Leaf_Curl.jpg",
+    "actual": "Leaf Curl",
+    "predicted": "Leaf Curl",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/011_bacterial_blight_in_cotton.jpg",
+    "actual": "bacterial_blight_in_cotton",
+    "predicted": "bacterial_blight_in_cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/012_Leaf_Curl.png",
+    "actual": "Leaf Curl",
+    "predicted": "Leaf Curl",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/013_maize_stem_borer.jpg",
+    "actual": "maize stem borer",
+    "predicted": "maize stem borer",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/014_American_Bollworm_on_Cotton.jpg",
+    "actual": "American Bollworm on Cotton",
+    "predicted": "American Bollworm on Cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/015_Wilt.jpg",
+    "actual": "Wilt",
+    "predicted": "Wilt",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/016_pink_bollworm_in_cotton.jpg",
+    "actual": "pink bollworm in cotton",
+    "predicted": "pink bollworm in cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/017_Cotton_Aphid.jpg",
+    "actual": "Cotton Aphid",
+    "predicted": "Cotton Aphid",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/018_Leaf_Curl.jpg",
+    "actual": "Leaf Curl",
+    "predicted": "Leaf Curl",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/019_cotton_mealy_bug.jpg",
+    "actual": "cotton mealy bug",
+    "predicted": "cotton mealy bug",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/020_Healthy_cotton.jpg",
+    "actual": "Healthy cotton",
+    "predicted": "Healthy cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/021_Leaf_Curl.jpg",
+    "actual": "Leaf Curl",
+    "predicted": "Leaf Curl",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/022_Flag_Smut.jpg",
+    "actual": "Flag Smut",
+    "predicted": "Flag Smut",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/023_Healthy_cotton.jpg",
+    "actual": "Healthy cotton",
+    "predicted": "Healthy cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/024_bollworm_on_Cotton.jpg",
+    "actual": "bollworm on Cotton",
+    "predicted": "American Bollworm on Cotton",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/025_Cotton_Aphid.jpg",
+    "actual": "Cotton Aphid",
+    "predicted": "Cotton Aphid",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/026_bollworm_on_Cotton.jpg",
+    "actual": "bollworm on Cotton",
+    "predicted": "American Bollworm on Cotton",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/027_cotton_mealy_bug.jpg",
+    "actual": "cotton mealy bug",
+    "predicted": "cotton mealy bug",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/028_Gray_Leaf_Spot.jpg",
+    "actual": "Gray_Leaf_Spot",
+    "predicted": "Common_Rust",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/029_American_Bollworm_on_Cotton.jpg",
+    "actual": "American Bollworm on Cotton",
+    "predicted": "American Bollworm on Cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/030_maize_fall_armyworm.jpg",
+    "actual": "maize fall armyworm",
+    "predicted": "maize fall armyworm",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/031_bacterial_blight_in_cotton.jpg",
+    "actual": "bacterial_blight_in_cotton",
+    "predicted": "bacterial_blight_in_cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/032_thirps_on__cotton.jpg",
+    "actual": "thirps on  cotton",
+    "predicted": "thirps on  cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/033_Sugarcane_Healthy.jpeg",
+    "actual": "Sugarcane Healthy",
+    "predicted": "Sugarcane Healthy",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/034_Wheat_aphid.jpg",
+    "actual": "Wheat aphid",
+    "predicted": "Wheat mite",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/035_Sugarcane_Healthy.jpeg",
+    "actual": "Sugarcane Healthy",
+    "predicted": "Sugarcane Healthy",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/036_Wheat_scab.jpg",
+    "actual": "Wheat scab",
+    "predicted": "Wheat scab",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/037_maize_ear_rot.jpg",
+    "actual": "maize ear rot",
+    "predicted": "maize ear rot",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/038_Leaf_Curl.jpg",
+    "actual": "Leaf Curl",
+    "predicted": "Healthy cotton",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/039_Wheat___Yellow_Rust.jpg",
+    "actual": "Wheat___Yellow_Rust",
+    "predicted": "Wheat___Yellow_Rust",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/040_cotton_whitefly.jpg",
+    "actual": "cotton whitefly",
+    "predicted": "cotton whitefly",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/041_American_Bollworm_on_Cotton.jpeg",
+    "actual": "American Bollworm on Cotton",
+    "predicted": "American Bollworm on Cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/042_Anthracnose_on_Cotton.jpg",
+    "actual": "Anthracnose on Cotton",
+    "predicted": "Anthracnose on Cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/043_maize_fall_armyworm.jpg",
+    "actual": "maize fall armyworm",
+    "predicted": "maize fall armyworm",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/044_bacterial_blight_in_rice.jpg",
+    "actual": "bacterial_blight_in_rice",
+    "predicted": "Wheat leaf blight",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/045_Gray_Leaf_Spot.jpg",
+    "actual": "Gray_Leaf_Spot",
+    "predicted": "RedRot sugarcane",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/046_Tungro.JPG",
+    "actual": "Tungro",
+    "predicted": "Tungro",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/047_Wilt.jpg",
+    "actual": "Wilt",
+    "predicted": "Wilt",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/048_Leaf_smut.jpg",
+    "actual": "Leaf smut",
+    "predicted": "Healthy cotton",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/049_Healthy_Maize.jpg",
+    "actual": "Healthy Maize",
+    "predicted": "Healthy Maize",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/050_Wheat_Brown_leaf_rust.jpg",
+    "actual": "Wheat Brown leaf rust",
+    "predicted": "Flag Smut",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/051_red_cotton_bug.jpg",
+    "actual": "red cotton bug",
+    "predicted": "red cotton bug",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/052_Leaf_smut.jpg",
+    "actual": "Leaf smut",
+    "predicted": "Tungro",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/053_maize_fall_armyworm.jpg",
+    "actual": "maize fall armyworm",
+    "predicted": "maize fall armyworm",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/054_RedRot_sugarcane.jpg",
+    "actual": "RedRot sugarcane",
+    "predicted": "Leaf smut",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/055_Cotton_Aphid.jpg",
+    "actual": "Cotton Aphid",
+    "predicted": "Cotton Aphid",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/056_Mosaic_sugarcane.jpg",
+    "actual": "Mosaic sugarcane",
+    "predicted": "Wheat powdery mildew",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/057_Wheat_mite.jpg",
+    "actual": "Wheat mite",
+    "predicted": "Wheat mite",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/058_pink_bollworm_in_cotton.jpg",
+    "actual": "pink bollworm in cotton",
+    "predicted": "pink bollworm in cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/059_Mosaic_sugarcane.jpg",
+    "actual": "Mosaic sugarcane",
+    "predicted": "RedRot sugarcane",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/060_Mosaic_sugarcane.jpeg",
+    "actual": "Mosaic sugarcane",
+    "predicted": "Mosaic sugarcane",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/061_Army_worm.jpg",
+    "actual": "Army worm",
+    "predicted": "Army worm",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/062_bacterial_blight_in_cotton.jpg",
+    "actual": "bacterial_blight_in_cotton",
+    "predicted": "bacterial_blight_in_cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/063_Wheat_Brown_leaf_rust.jpg",
+    "actual": "Wheat Brown leaf rust",
+    "predicted": "Wheat black rust",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/064_Cotton_Aphid.jpg",
+    "actual": "Cotton Aphid",
+    "predicted": "Wilt",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/065_Wheat_Stem_fly.jpg",
+    "actual": "Wheat Stem fly",
+    "predicted": "Wheat Stem fly",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/066_Wheat_Brown_leaf_rust.jpg",
+    "actual": "Wheat Brown leaf rust",
+    "predicted": "Wheat black rust",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/067_maize_stem_borer.jpg",
+    "actual": "maize stem borer",
+    "predicted": "maize stem borer",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/068_bacterial_blight_in_cotton.jpg",
+    "actual": "bacterial_blight_in_cotton",
+    "predicted": "Wilt",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/069_Common_Rust.jpg",
+    "actual": "Common_Rust",
+    "predicted": "Common_Rust",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/070_Wheat_leaf_blight.JPG",
+    "actual": "Wheat leaf blight",
+    "predicted": "Wheat leaf blight",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/071_Anthracnose_on_Cotton.jpg",
+    "actual": "Anthracnose on Cotton",
+    "predicted": "Anthracnose on Cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/072_RedRot_sugarcane.jpg",
+    "actual": "RedRot sugarcane",
+    "predicted": "RedRot sugarcane",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/073_Cotton_Aphid.jpg",
+    "actual": "Cotton Aphid",
+    "predicted": "Cotton Aphid",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/074_Wheat_Stem_fly.jpg",
+    "actual": "Wheat Stem fly",
+    "predicted": "Wheat Stem fly",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/075_cotton_mealy_bug.jpg",
+    "actual": "cotton mealy bug",
+    "predicted": "cotton mealy bug",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/076_cotton_mealy_bug.jpeg",
+    "actual": "cotton mealy bug",
+    "predicted": "cotton mealy bug",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/077_bacterial_blight_in_rice.jpg",
+    "actual": "bacterial_blight_in_rice",
+    "predicted": "maize ear rot",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/078_Wheat_Brown_leaf_rust.jpg",
+    "actual": "Wheat Brown leaf rust",
+    "predicted": "Wheat Brown leaf Rust",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/079_Rice_Blast.jpg",
+    "actual": "Rice Blast",
+    "predicted": "Wheat black rust",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/080_cotton_whitefly.jpg",
+    "actual": "cotton whitefly",
+    "predicted": "cotton whitefly",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/081_pink_bollworm_in_cotton.gif",
+    "actual": "pink bollworm in cotton",
+    "predicted": "pink bollworm in cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/082_Wheat_mite.jpg",
+    "actual": "Wheat mite",
+    "predicted": "Rice Blast",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/083_RedRot_sugarcane.jpg",
+    "actual": "RedRot sugarcane",
+    "predicted": "RedRot sugarcane",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/084_Sugarcane_Healthy.jpeg",
+    "actual": "Sugarcane Healthy",
+    "predicted": "Sugarcane Healthy",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/085_RedRot_sugarcane.jpg",
+    "actual": "RedRot sugarcane",
+    "predicted": "RedRot sugarcane",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/086_Wheat_black_rust.jpg",
+    "actual": "Wheat black rust",
+    "predicted": "Wheat black rust",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/087_Gray_Leaf_Spot.jpg",
+    "actual": "Gray_Leaf_Spot",
+    "predicted": "Gray_Leaf_Spot",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/088_Healthy_Maize.JPG",
+    "actual": "Healthy Maize",
+    "predicted": "Healthy Maize",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/089_bacterial_blight_in_rice.jpg",
+    "actual": "bacterial_blight_in_rice",
+    "predicted": "Wheat powdery mildew",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/090_maize_fall_armyworm.jpg",
+    "actual": "maize fall armyworm",
+    "predicted": "maize fall armyworm",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/091_Leaf_Curl.jpg",
+    "actual": "Leaf Curl",
+    "predicted": "Leaf Curl",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/092_bacterial_blight_in_rice.jpg",
+    "actual": "bacterial_blight_in_rice",
+    "predicted": "Wheat aphid",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/093_Healthy_cotton.jpg",
+    "actual": "Healthy cotton",
+    "predicted": "cotton whitefly",
+    "result": "Incorrect"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/094_thirps_on__cotton.jpg",
+    "actual": "thirps on  cotton",
+    "predicted": "thirps on  cotton",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/095_Tungro.JPG",
+    "actual": "Tungro",
+    "predicted": "Tungro",
+    "result": "Correct"
+  },
+  {
+    "image": "knn_cnn_outputs/demo_pool/096_Cotton_Aphid.jpg",
+    "actual": "Cotton Aphid",
+    "predicted": "Cotton Aphid",
+    "result": "Correct"
+  }
+];
